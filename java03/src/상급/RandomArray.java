2238055b29098feb198c0fbdc97d5b13d2b75604
gurwo0535 <gurwo0535@naver.com>
2024-05-30 18:08:18 +0900

    0530

---
+++ java03/src/상급/RandomArray.java
@@ -0,0 +1,32 @@
+package 상급;
+
+import java.util.Scanner;
+
+public class RandomArray {
+
+	public static void main(String[] args) {
+
+		System.out.println("정수의 개수 : ");
+		Scanner scanner = new Scanner(System.in);
+		
+		int num = scanner.nextInt();
+		for (int i = 0; i < num.length; i++) {
+			 int random = (int)(Math.random()*100)+1;
+			System.out.println(random);
+			
+		}
+		
+	}
+
+}
+
+
+
+//정수를몇개저장할지키보드로부터개수를입력받아
+//(100이하 개수) 정수 배열을 생성하고, 이곳에 1에서
+//100까지 범위의 정수를랜덤하게삽입하라.
+// *.출력
+// 정수몇개?24
+// 48 29 69 57 95 21 11 91 37 63 
+// 56 88 20 6 81 8 4 24 7 65 
+// 1 89 72 90
---
+++ java03/src/중상급/StringTokenizer숙제있음.java
@@ -0,0 +1,97 @@
+package 중상급;
+
+import java.util.Iterator;
+
+// 이거 안됨 외워 씨발 병신아 제발 
+//String text = ["홍길동","장화","홍련","콩쥐","팝쥐"];
+
+
+// 이 방식으로 풀어보자 
+//public class StringTokenizer숙제있음 {
+//	public static void main(String[] args) {
+//		String text = "홍길동,장화,홍련,콩쥐,팝쥐";
+//		
+//		
+//		
+//		// replace() 사용법 문자 두가지의 교환 
+//		text = text.replace(",", "\n");
+//		
+//		System.out.println(text);
+//		
+//		
+//		
+//	}
+//}		
+		// ---------- 형 ----------- 
+
+//		String text = "홍길동,장화,홍련,콩쥐,팝쥐";
+//		String tmp = "";
+//		for (int i = 0; i < text.length(); i++) {
+//			if(",".equals(Character.toString(text.charAt(i)))) {
+//				tmp += "\n";
+//			}else {
+//				tmp += text.charAt(i);
+//			}
+//			
+//		}
+//		System.out.println(tmp);
+//		
+//	}
+//	}
+
+//	String text = "홍길동,장화,홍련,콩쥐,팝쥐";
+//
+//	
+//	for (int i = 0; i < text.length(); i++) {
+//		if (text.equals(",")) {
+//			text = "\n";
+//		}
+//		System.out.println(text);}
+
+// ======================= 형 ======================
+public class StringTokenizer숙제있음 {
+	public static void main(String[] args) {
+
+		String text = "홍길동,장화,홍련,콩쥐,팝쥐";
+		
+		//split() :공백을 기준으로 문자열 나누기
+        String[] names = text.split(",");
+
+        // 배열의 각 요소를 출력
+        for (String name : names) {
+            System.out.println(name);
+        }
+		
+	}
+}
+// =========== 선생님 ===============================
+	// StringTokenizer클래스 활용법 
+	// hasMoreTokens() 연계로 사용 
+//
+//import java.util.StringTokenizer;
+//
+//public class StringTokenizerEx {
+//	
+//	public static void main(String[] args) {
+//		// st 객체는 5개의 토큰을 가진다 
+//		StringTokenizer st = 
+// new StringTokenizer("홍길동/장화/홍련/콩쥐/팥쥐", "/");
+//		
+//		
+//		// st.hasMoreTokens() :토큰이 있으면 true
+//		while (st.hasMoreTokens()) {
+//			//st.nextToken(): 토큰을 꺼내온다
+//			System.out.println(st.nextToken()); 
+//			
+//		}
+//	}
+//}
+
+
+//“홍길동/장화/홍련/콩쥐/팥쥐”문자열을‘/’를 
+//구분 문자로 하여 토큰을분리하여각토큰을출력하라.
+//홍길동
+//장화
+//홍련
+//콩쥐
+//팥쥐
